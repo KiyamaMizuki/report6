3e491b5847373d900e91f6176d4230ec3e78d490
KiyamaMizuki <e185758@ie.u-ryukyu.ac.jp>
2019-01-27 17:37:29 +0900

    add Bingo_ball

--- src/main/java/jp/ac/uryukyu/ie/e185758/Bingo.java
+++ src/main/java/jp/ac/uryukyu/ie/e185758/Bingo.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Bingo {
-    ArrayList<Integer> rand() {
+    int[][] card;
+
+    ArrayList<Integer> rand(int min,int max) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         // listに値を入れる。この段階では昇順
-        for (int i = 1; i <= 9; i++) {
+        for (int i = min; i <= max; i++) {
             list.add(i);
         }
         // シャッフルして、順番を変える
@@ -15,32 +17,79 @@ public class Bingo {
         return list;
     }
 
-    void Bingo() {
-        /*
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        // listに値を入れる。この段階では昇順
-        for (int i = 1; i <= 9; i++) {
-            list.add(i);
-        }
-        // シャッフルして、順番を変える
-        Collections.shuffle(list);
-        */
+    int[][] Bingo() {
+        //ArrayList<Integer> new_rand;
+        ArrayList new_rand_1 = rand(1,15);
+        ArrayList new_rand_2 = rand(16,30);
+        ArrayList new_rand_3 = rand(31,45);
+        ArrayList new_rand_4 = rand(46,60);
+        ArrayList new_rand_5 = rand(61,75);
 
         int[][] bingocard;
-        bingocard = new int[3][3];
+        bingocard = new int[5][5];
         int number = 0;
-        for (int vertical = 0; vertical < 3; vertical++) {
-            for (int i = 0; i < 3; i++) {
-                int number_value = 1;
-                number++;
-                bingocard[vertical][i] = number_value;
+        for (int vertical = 0; vertical < 5; vertical++) {
+            for (int i = 0; i < 5; i++) {
+
+                if(i == 0){
+                    //new_rand_1 = rand(1,15);
+                    bingocard[vertical][i] =  (int) new_rand_1.get(number);
+                }else if(i == 1){
+                    //new_rand_2 = rand(16,30);
+                    bingocard[vertical][i] =  (int) new_rand_2.get(number);
+                }else if(i == 2) {
+                    //new_rand_3 = rand(31, 45);
+                    bingocard[vertical][i] =  (int) new_rand_3.get(number);
+                }else if(i == 3) {
+                    //new_rand_4 = rand(46, 60);
+                    bingocard[vertical][i] =  (int) new_rand_4.get(number);
+                }else {
+                    //new_rand_5 = rand(61,75);
+                    bingocard[vertical][i] =  (int) new_rand_5.get(number);
+                }
+
+                //bingocard[vertical][i] =  (int) new_rand.get(number);
+                //number++;
                 int value = bingocard[vertical][i];
+                System.out.print("|" + String.format("%02d", value) + "|");
+
+            }
+            System.out.println(" ");
+            number++;
+        }
+        card = bingocard;
+        return card;
+    }
+
+    void Bingo_ball(){
+
+        boolean Judg = false;
+        int number = 0;
+        ArrayList rand = rand(1,75);
+        int ball = (int) rand.get(number);
+        System.out.println("n回目の数字は" + ball + "です。");
+
+        
+        for (int vertical = 0; vertical < 5; vertical++) {
+            for (int i = 0; i < 5; i++) {
+                if(card[vertical][i] == ball) {
+                    card[vertical][i] = 0;
+                    Judg = true;
+                }
+
+                int value = card[vertical][i];
                 System.out.print("|" + String.format("%02d", value) + "|");
             }
             System.out.println(" ");
         }
-    }
-    void Bingo_ball(){
+        if(Judg) {
+            System.out.println("おめでとうございます。");
+        }else{
+            System.out.print("該当する数字はありませんでした。");
+        }
+
+
+
 
     }
 }
--- src/main/java/jp/ac/uryukyu/ie/e185758/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185758/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String args[]){
         Bingo bingo = new Bingo();
         bingo.Bingo();
-        //bingo.Bingo_ball();
+        bingo.Bingo_ball();
     }
 }
